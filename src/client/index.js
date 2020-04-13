3939f5ed2706a911d59a1a2911f1997f1c303480
Christina Hebets <chebets@tucson.local>
2020-04-13 08:57:31 -0700

    Playing around with async. Put functionality back to starting point where lat and long undefined.

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,5 @@ import { performActionWeather } from './js/app'
 import { init } from './js/app'
 import './styles/style.scss'
 
-document.getElementById('generate').addEventListener('click', init);
-//document.getElementById('generate').addEventListener('click', performActionWeather);
+//document.getElementById('generate').addEventListener('click', init);
+document.getElementById('generate').addEventListener('click', performAction);
